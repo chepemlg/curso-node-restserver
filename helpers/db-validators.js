43cb6685dcf0793a56ec8c1c4913a489246c1db8
Jose Lopez <jose.lg@alliedglobal.com>
2022-07-15 00:03:18 -0600

    File upload added

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -41,6 +41,19 @@ const existeProductoPorId = async (id ) => {
   }
 }
 
+/**
+ * Validar coleccion permitidas
+ */
+const coleccionesPermitidas = ( coleccion = '', colecciones = []) => {
+
+  const incluida = colecciones.includes(coleccion);
+  if ( !incluida ) {
+    throw new Error(`La colección ${ coleccion } no es permitida, ${colecciones}`);
+  }
+
+  return true;
+
+}
 
 
 module.exports = {
@@ -48,6 +61,7 @@ module.exports = {
   emailExiste,
   existeUsuarioPorId,
   existeCategoriaPorId,
-  existeProductoPorId
+  existeProductoPorId,
+  coleccionesPermitidas
 }
 
--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-var cors = require('cors');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const { dbConnection } = require('../database/config');
 
 class Server {
@@ -14,6 +15,7 @@ class Server {
         categorias: '/api/categorias',
         productos:  '/api/productos',
         usuarios:   '/api/usuarios',
+        uploads:   '/api/uploads',
 
       }
 
@@ -42,14 +44,24 @@ class Server {
         //Directorio Público
         this.app.use( express.static('public') );
 
+        // Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+          useTempFiles : true,
+          tempFileDir : '/tmp/',
+          createParentPath: true
+        }));
+
     }
 
     routes() {
+
         this.app.use(this.paths.auth, require('../routes/auth'));
         this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
+    
     }
 
     listen() {
